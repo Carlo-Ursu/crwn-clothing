3ae4ec7cf182feb5335b9c0d9861706b38fab3e2
Carlo Ursu <carloursu6@gmail.com>
2023-04-21 17:37:02 +0300

    Added redux for the category

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,9 +2,20 @@ import CategoriesPreview from '../categories-preview/categories-preview.componen
 import Category from "../category/category.component";
 
 import {Route, Routes} from "react-router-dom";
+import PRODUCTS from "../../shop-data";
+import {useDispatch} from "react-redux";
+import {setCategories} from "../../store/categories/category.action";
 
 
 const Shop = () => {
+    const dispatch = useDispatch();
+
+    const categoryArray = PRODUCTS.map(category => category);
+
+
+    dispatch(setCategories(categoryArray));
+
+
     return (
         <Routes>
             <Route index element={<CategoriesPreview/>}/>
