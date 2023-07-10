968374293a69b53117fa04f3ca2bc911a4ec2380
Carlo Ursu <carloursu6@gmail.com>
2023-07-10 18:12:30 +0300

    Added Stripe feature for payments

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,34 +1,27 @@
-import { ReactComponent as GmailLogo } from '../../assets/google.svg';
-import { useNavigate } from 'react-router-dom';
-
-
-import {
-  signInWithGooglePopUp,
-} from '../../utils/firebase/firebase.utils';
-
-import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
-
+import { ReactComponent as GmailLogo } from "../../assets/google.svg";
+import { useNavigate } from "react-router-dom";
+import { signInWithGooglePopUp } from "../../utils/firebase/firebase.utils";
+import { SignInContainer, ButtonsContainer } from "./sign-in-form.styles";
 
 const SignInForm = () => {
-
   const navigation = useNavigate();
 
   const signInWithGoogle = async () => {
     try {
       await signInWithGooglePopUp();
-      navigation('/shop');  
+      navigation("/shop");
     } catch (error) {
-      console.log('user sign in failed', error);
+      console.log("User sign-in failed", error);
     }
   };
-  
+
   return (
     <SignInContainer>
       <h2>Welcome to CRWN CLOTHING</h2>
       <span>Sign in with Gmail</span>
       <form>
         <ButtonsContainer>
-            <GmailLogo onClick={signInWithGoogle} />
+          <GmailLogo onClick={signInWithGoogle} />
         </ButtonsContainer>
       </form>
     </SignInContainer>
--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 // import { PersistGate } from 'redux-persist/integration/react';
 // import { persistor } from './store/store';
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
-        {/* <PersistGate persistor={persistor}> */}
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        {/* </PersistGate> */}
-    </Provider>
+  <Provider store={store}>
+    {/* <PersistGate persistor={persistor}> */}
+    <BrowserRouter>
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
+    </BrowserRouter>
+    {/* </PersistGate> */}
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
